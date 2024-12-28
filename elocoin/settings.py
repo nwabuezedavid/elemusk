2d7d400a09ea642d7aa860184774407201845c2b
NwabuezeDavid <davidnwabueze333@gmail.com>
2024-12-28 22:31:27 +0100

    first commit

--- elocoin/settings.py
+++ elocoin/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app.auth_middleware.AuthenticationMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'elocoin.urls'
